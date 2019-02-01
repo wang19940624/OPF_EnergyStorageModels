2187b4273253132eed273d7463dc528ef7be9244
Noah Rhodes <noahx23@gmail.com>
2019-02-01 17:49:30 -0600

    Finished Economic Dispatch Model

--- economic_dispatch.jl
+++ economic_dispatch.jl
@@ -68,7 +68,7 @@ model = Model(solver = nlp_solver)
 # Add power generation variables for each generator, including bounds
 @variable(model, ref[:gen][i]["pmin"] <= pg[i in keys(ref[:gen])] <= ref[:gen][i]["pmax"])
 # Add power flow variables for branches
-@variable(model, ref[:branch][l]["rate_a"] <= p[(l,i,j) in ref[:arcs]] <= ref[:branch][l]["rate_a"])
+@variable(model, -ref[:branch][l]["rate_a"] <= p[(l,i,j) in ref[:arcs]] <= ref[:branch][l]["rate_a"])
 
 # Add Objective Function
 # ----------------------
@@ -82,14 +82,35 @@ model = Model(solver = nlp_solver)
 # Add Constraints
 # ---------------
 
-# Power balance constraints
-for (i,bus) in ref[Ibus]
+# Nodal power balance constraints
+for (i,bus) in ref[:bus]
     # Build a list of the loads and shunt elements connected to the bus i
     bus_loads = [ref[:load][l] for l in ref[:bus_loads][i]]
     bus_shunts = [ref[:shunt][s] for s in ref[:bus_shunts][i]]
 
     # Power balance
+    @constraint(model,
+        sum(p[a] for a in ref[:bus_arcs][i]) == # sum of power flow on lines from bus i
+        sum(pg[g] for g in ref[:bus_gens][i]) - # sum of power generation at bus i
+        sum(load["pd"] for load in bus_loads)   # sum of active load consumption at bus i
+    )
+end
 
+# Branch flow limits
+for (i, branch) in ref[:branch]
+    # build the from variable id of the i-th branch
+    f_idx = (i, branch["f_bus"], branch["t_bus"])
+    # build the to variable id of the i-th branch
+    t_idx = (i, branch["t_bus"], branch["f_bus"])
+     # note: it is necessary to distinguish between the from and to sides of a branch due to power losses
+
+     p_fr = p[f_idx]
+     p_to = p[t_idx]
+     # note: p_fr, p_to are references to the optimization variable p[index]
+
+     @constraint( model, p_fr + p_to == 0)
+
+end
 
 ###############################################################################
 # 3. Solve the Optimal Power Flow Model and Review the Results
