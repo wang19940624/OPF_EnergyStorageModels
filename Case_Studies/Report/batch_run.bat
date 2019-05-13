312a908a332820ca2b313127c76dfc39d804b1eb
Noah Rhodes <noahx23@gmail.com>
2019-05-12 23:56:32 -0500

    Final Scenarios run

--- Case_Studies/Report/batch_run.bat
+++ Case_Studies/Report/batch_run.bat
@@ -1,10 +1,10 @@
 ::julia ACOPF.jl
-::julia ACOPF_Battery_Full.jl
+julia ACOPF_Battery_Full.jl
 ::julia ACOPF_Battery_Limited.jl
-::julia ACOPF_Flywheel_Full.jl
+julia ACOPF_Flywheel_Full.jl
 ::julia ACOPF_Flywheel_Limited.jl
 julia ACOPF_Torque_Full.jl
-julia ACOPF_Torque_Limited.jl
-julia ACOPF_Torque_Stop_Full.jl
-julia ACOPF_Torque_Stop_Limited.jl
+::julia ACOPF_Torque_Limited.jl
+::julia ACOPF_Torque_Stop_Full.jl
+::julia ACOPF_Torque_Stop_Limited.jl
 
